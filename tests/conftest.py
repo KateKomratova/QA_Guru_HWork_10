4c61584148ab8bc37f25dd154f610cadfd59f001
Ekaterina Komratova <ekaterina.komratova@gmail.com>
2025-05-08 01:11:36 +0300

    изменен conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,53 @@
 import pytest
+import os
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
 from utils import attach
 
+# Загрузка переменных окружения из .env
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+        load_dotenv()
+
 @pytest.fixture(scope="function")
-def browser_conf():
-    print("\nSetting up browser configuration...")
-    browser.config.driver_name = 'edge'
-    browser.config.timeout = 10
+def browser_conf(load_env):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
-    yield
-    print("\nTearing down browser...")
-    browser.quit()
 
-attach.add_screenshot(browser)
-attach.add_logs(browser)
-attach.add_html(browser)
-attach.add_video(browser)
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    # Получаем данные из переменных окружения
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    # Создание удалённого драйвера
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
